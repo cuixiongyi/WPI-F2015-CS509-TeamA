6817a962c1bac0137c918a0c2dc262bac808ca05
Nick <nkal123@gmail.com>
2015-10-04 14:24:04 -0400

    Removes the getDistance function (this should be in node). Also adds function header comments

--- BaseModel/src/com/wpi/cs509/teamA/misc/Coordinate.java
+++ BaseModel/src/com/wpi/cs509/teamA/misc/Coordinate.java
@@ -7,35 +7,51 @@ public class Coordinate {
 	private int x;
 	private int y;
 
+	/**
+	 * Default constructor
+	 */ 
 	public Coordinate(){
 		
 	}
 	
+	/**
+	 * Constructor assigning x and y coordinates
+	 * @param x The x coordinate for the coordinate pair
+	 * @param y The y coordinate for the coordinate pair
+	 */ 
 	public Coordinate(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
-	
-	// calculate the distance between this node and the anotherNode
-	public void getDistance(Node anotherNode){
-		
-		// x = this.x - anotherNode.getLocation().getX();
-		// same to y
-		
-	}
 
+	/**
+	 * Gets the x coordinate of the coordinate pair
+	 * @return x coordinate
+	 */ 
 	public int getX() {
 		return x;
 	}
 
+	/**
+	 * Sets the x coordinate of the coordinate pair
+	 * @param x The new x coordinate value
+	 */ 
 	public void setX(int x) {
 		this.x = x;
 	}
 
+	/**
+	 * Gets the y coordinate of the coordinate pair
+	 * @return y coordinate
+	 */ 
 	public int getY() {
 		return y;
 	}
 
+	/**
+	 * Sets the y coordinate of the coordinate pair
+	 * @param y The new y coordinate value
+	 */ 
 	public void setY(int y) {
 		this.y = y;
 	}
