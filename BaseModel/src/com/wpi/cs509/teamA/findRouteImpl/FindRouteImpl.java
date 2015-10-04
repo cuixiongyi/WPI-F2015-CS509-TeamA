c02100af7010e7e35b1553c852c33284302eef14
Nick <nkal123@gmail.com>
2015-10-04 14:03:03 -0400

    Update FindRouteImpl.java
    Adds function header comments

--- BaseModel/src/com/wpi/cs509/teamA/findRouteImpl/FindRouteImpl.java
+++ BaseModel/src/com/wpi/cs509/teamA/findRouteImpl/FindRouteImpl.java
@@ -7,11 +7,22 @@ import com.wpi.cs509.teamA.findRoute.FindRoute;
 
 public class FindRouteImpl implements FindRoute{
 
-	
+	/** 
+	 * Finds a route from a source to destination on the same map
+	 * @param startNode Starting location of the route 
+	 * @param endNode Ending location of the route
+	 * @return Route from source to destination
+	 */
 	public List<Node> findRouteSameMap(Node startNode, Node endNode){
 		return null;
 	}
 	
+	/** 
+	 * Finds a route from a source to destination on multiple maps
+	 * @param startNode Starting location of the route 
+	 * @param endNode Ending location of the route
+	 * @return Route from source to destination across multiple maps
+	 */
 	public List<List<Node>> findRouteMultipleMap(Node startNode, Node endNode){
 		return null;
 	}
