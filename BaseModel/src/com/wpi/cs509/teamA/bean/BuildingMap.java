904f13c08837fb6e16dccd281bf0a5749b31dad6
Jie <wemadevictory@gmail.com>
2015-10-10 18:51:51 -0400

    considering if we need to remove BuildingMap

--- BaseModel/src/com/wpi/cs509/teamA/bean/BuildingMap.java
+++ BaseModel/src/com/wpi/cs509/teamA/bean/BuildingMap.java
@@ -1,34 +1,37 @@
 package com.wpi.cs509.teamA.bean;
 
-
 /**
  * mark all the places in a building
+ * 
  * @author CS509-Team A
  * @version Oct 4th
  */
-public class BuildingMap extends GeneralMap{
-	
-	/** the name of the building*/
+public class BuildingMap extends GeneralMap {
+
+	/** the name of the building */
 	private String buildingName;
-	
-	public BuildingMap(){
+
+	public BuildingMap() {
 		System.out.println("initialize building map...");
 	}
 
 	/**
 	 * get the name of the building
+	 * 
 	 * @return name of the building
 	 */
 	public String getBuildingName() {
 		return buildingName;
 	}
-	
+
 	/**
 	 * set the name of the building
-	 * @param name of the building
+	 * 
+	 * @param name
+	 *            of the building
 	 */
 	public void setBuildingName(String buildingName) {
 		this.buildingName = buildingName;
 	}
-	
+
 }
