40f4a1703aaac9ae8bd5a635ba148825810568cf
Xiongyi Cui <cuixiongyi2013@gmail.com>
2015-10-13 14:59:04 -0400

    change

--- BaseModel/src/com/wpi/cs509/teamA/ui/StateMouseListener.java
+++ BaseModel/src/com/wpi/cs509/teamA/ui/StateMouseListener.java
@@ -3,7 +3,7 @@ package com.wpi.cs509.teamA.ui;
 import java.awt.event.MouseListener;
 
 /**
- * The state interface has two implementations. The state’s method has a
+ * The state interface has two implementations. All the methods have a
  * reference to the context object and is able to change its state.
  * 
  * @author CS 509-Team A
