11555458b5a229436e4027b6ad5ba2b1484ec160
xiongkuang <xiongkuang@gmail.com>
2015-11-04 19:39:41 -0500

    new layout complete

--- BaseModel/src/com/wpi/cs509/teamA/ui/InputPanel.java
+++ BaseModel/src/com/wpi/cs509/teamA/ui/InputPanel.java
@@ -1,7 +1,13 @@
 package com.wpi.cs509.teamA.ui;
 
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -23,11 +29,14 @@ public class InputPanel extends JPanel {
 	private JTextField endPoint;
 	private JButton btnSearch;
 	private JButton adminLogin;
+	private JLabel lblFrom;
+	private JLabel lblTo;
+	
 
 	private final static String START = "Start";
 	private final static String END = "End";
 	private final static String SEARCH = "Search";
-	private final static String LOGIN = "Login as Admin";
+	private final static String LOGIN = "Login";
 	private final static String TO = "To: ";
 	private final static String FROM = "From: ";
 
@@ -35,29 +44,100 @@ public class InputPanel extends JPanel {
 	 * Constructor. Initialize all the input panel.
 	 */
 	public InputPanel() {
-		// User input block
-		this.startPoint = new JTextField(START);
-		this.endPoint = new JTextField(END);
+//		// User input block
+		this.startPoint = new JTextField();
+		this.endPoint = new JTextField();
 		this.btnSearch = new JButton(SEARCH);
 		this.adminLogin = new JButton(LOGIN);
 
 		this.setLayout(null);
-		this.add(new JLabel(TO));
 		this.add(startPoint);
-		this.add(new JLabel(FROM));
 		this.add(endPoint);
 		this.add(btnSearch);
 		this.add(adminLogin);
 
-		this.getAdminLogin().setFont(new Font("Arial", Font.PLAIN, 20));
-		this.getBtnSearch().setFont(new Font("Arial", Font.PLAIN, 25));
-		this.getEndPoint().setFont(new Font("Arial", Font.PLAIN, 22));
-		this.getStartPoint().setFont(new Font("Arial", Font.PLAIN, 22));
-		this.getAdminLogin().setBounds(981, 0, 196, 29);
-		this.getBtnSearch().setBounds(603, 69, 132, 89);
-		this.getEndPoint().setBounds(200, 120, 310, 38);
-		this.getStartPoint().setBounds(200, 69, 310, 38);
+		this.getAdminLogin().setFont(new Font("Arial", Font.PLAIN, 12));
+		this.getBtnSearch().setFont(new Font("Arial", Font.PLAIN, 15));
+		this.getEndPoint().setFont(new Font("Arial", Font.PLAIN, 12));
+		this.getStartPoint().setFont(new Font("Arial", Font.PLAIN, 12));
+		this.getAdminLogin().setBounds(210, 0, 60, 30);
+		this.getBtnSearch().setBounds(50, 300, 150, 38);
+		this.getEndPoint().setBounds(50, 200, 150, 38);
+		this.getStartPoint().setBounds(50, 100, 150, 38);
 		this.setBounds(0, 0, 1178, 214);
+		
+		lblFrom = new JLabel(FROM);
+		lblFrom.setBounds(15, 110, 61, 16);
+		add(lblFrom);
+		
+		lblTo = new JLabel(TO);
+		lblTo.setBounds(30, 210, 61, 16);
+		add(lblTo);
+		
+//		
+//		setBounds(100, 100, 200, 450);
+//		GridBagLayout gridBagLayout = new GridBagLayout();
+//		gridBagLayout.columnWidths = new int[]{50,50,50,50};
+//		gridBagLayout.rowHeights = new int[]{50,50,50,50,50,50,50,50,50};
+//		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+//		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0};
+//		setLayout(gridBagLayout);
+//		
+//		JButton adminLogin = new JButton("Log in");
+//		GridBagConstraints gbc_adminLogin = new GridBagConstraints();
+//		gbc_adminLogin.insets = new Insets(0, 0, 5, 0);
+//		gbc_adminLogin.gridx = 3;
+//		gbc_adminLogin.gridy = 0;
+//		add(adminLogin, gbc_adminLogin);
+//		
+//		lblFrom = new JLabel("From:");
+//		GridBagConstraints gbc_lblFrom = new GridBagConstraints();
+//		gbc_lblFrom.insets = new Insets(0, 0, 5, 5);
+//		gbc_lblFrom.anchor = GridBagConstraints.SOUTHWEST;
+//		gbc_lblFrom.gridx = 0;
+//		gbc_lblFrom.gridy = 1;
+//		add(lblFrom, gbc_lblFrom);
+//		
+//		lblTo = new JLabel("To:");
+//		GridBagConstraints gbc_lblTo = new GridBagConstraints();
+//		gbc_lblTo.insets = new Insets(0, 0, 0, 5);
+//		gbc_lblTo.anchor = GridBagConstraints.SOUTHWEST;
+//		gbc_lblTo.gridx = 0;
+//		gbc_lblTo.gridy = 3;
+//		add(lblTo, gbc_lblTo);
+//		
+//		JButton btnSearch = new JButton("Search");
+//		btnSearch.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//			}
+//		});
+//		GridBagConstraints gbc_btnSearch = new GridBagConstraints();
+//		gbc_btnSearch.insets = new Insets(0, 0, 0, 5);
+//		gbc_btnSearch.gridx = 3;
+//		gbc_btnSearch.gridy = 3;
+//		add(btnSearch, gbc_btnSearch);
+//		
+//		startPoint = new JTextField();
+//		GridBagConstraints gbc_startPoint = new GridBagConstraints();
+//		gbc_startPoint.anchor = GridBagConstraints.NORTH;
+//		gbc_startPoint.insets = new Insets(0, 0, 5, 5);
+//		gbc_startPoint.fill = GridBagConstraints.HORIZONTAL;
+//		gbc_startPoint.gridwidth = 3;
+//		gbc_startPoint.gridx = 0;
+//		gbc_startPoint.gridy = 2;
+//		add(startPoint, gbc_startPoint);
+//		startPoint.setColumns(10);
+//		
+//		endPoint = new JTextField();
+//		GridBagConstraints gbc_endPoint = new GridBagConstraints();
+//		gbc_endPoint.anchor = GridBagConstraints.NORTH;
+//		gbc_endPoint.insets = new Insets(0, 0, 0, 5);
+//		gbc_endPoint.fill = GridBagConstraints.HORIZONTAL;
+//		gbc_endPoint.gridwidth = 3;
+//		gbc_endPoint.gridx = 0;
+//		gbc_endPoint.gridy = 4;
+//		add(endPoint, gbc_endPoint);
+//		endPoint.setColumns(10);
 	}
 
 	/**
@@ -119,5 +199,4 @@ public class InputPanel extends JPanel {
 	public void setAdminLogin(JButton adminLogin) {
 		this.adminLogin = adminLogin;
 	}
-
 }
--- BaseModel/src/com/wpi/cs509/teamA/ui/UserScreen.java
+++ BaseModel/src/com/wpi/cs509/teamA/ui/UserScreen.java
@@ -3,11 +3,17 @@ package com.wpi.cs509.teamA.ui;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
 import javax.swing.plaf.basic.BasicArrowButton;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -25,6 +31,7 @@ public class UserScreen extends JFrame {
 
 	private static UserScreen userScreen;
 	private Container container;
+	private JPanel contentPane;
 	private ImageComponent imgComponent;
 	private JScrollPane imgScrollPanel;
 	private NeighborDialog neighborDialog;
@@ -45,9 +52,30 @@ public class UserScreen extends JFrame {
 
 		container = getContentPane();
 		// container.setLayout(new BorderLayout());
-
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0,0,0,0,0,0,0,0,0,0,0,0,30,30,30,50};
+		gbl_contentPane.rowHeights = new int[]{0};
+		gbl_contentPane.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		
 		// input panel and components
 		inputPanel = new InputPanel();
+		GridBagConstraints gbc_inputPanel = new GridBagConstraints();
+		gbc_inputPanel.gridwidth = 4;
+		gbc_inputPanel.insets = new Insets(0, 0, 5, 5);
+		gbc_inputPanel.fill = GridBagConstraints.BOTH;
+		gbc_inputPanel.gridx = 12;
+		gbc_inputPanel.gridy = 0;
+		contentPane.add(inputPanel, gbc_inputPanel);
+		
+		
 		// initialize neighborDialog to be used later
 		neighborDialog = new NeighborDialog();
 		neighborDialog.setVisible(false);
@@ -59,32 +87,65 @@ public class UserScreen extends JFrame {
 		imgComponent.setImagePath(System.getProperty("user.dir") + "/src/Final_Campus_Map.jpg");
 		imgComponent.setPreferredSize(new Dimension(imgComponent.getImgWidth(), imgComponent.getImgHeight()));
 		imgComponent.setVisible(true);
-		getContentPane().setLayout(null);
 		// add listener
 		// imgComponent.addMouseListener(imgComponent);
 
-		// scroll panel
-		imgScrollPanel = new JScrollPane();
-		imgScrollPanel.setBounds(181, 215, 834, 557);
+//		// scroll panel
+//		imgScrollPanel = new JScrollPane();
+//		imgScrollPanel.setBounds(181, 215, 834, 557);
+		JScrollPane imgScrollPanel = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 0, 5);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_inputPanel.gridwidth = 10;
+		gbc_inputPanel.gridheight = 10;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 0;
+		contentPane.add(imgScrollPanel, gbc_scrollPane);
 		imgScrollPanel.setPreferredSize(new Dimension(imgComponent.getImgWidth(), imgComponent.getImgHeight()));
-		// for scroll panel
+//		// for scroll panel
 		imgScrollPanel.setViewportView(imgComponent);
 		imgScrollPanel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		imgScrollPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		// button to change map
+		
+		
+		
+//		// button to change map
+//		BasicArrowButton WestArrowButton = new BasicArrowButton(BasicArrowButton.WEST);
+//		WestArrowButton.setBounds(109, 632, 57, 140);
+//
+//		BasicArrowButton EastArrowButton = new BasicArrowButton(BasicArrowButton.EAST);
+//		EastArrowButton.setBounds(1030, 632, 57, 140);
+		
 		BasicArrowButton WestArrowButton = new BasicArrowButton(BasicArrowButton.WEST);
-		WestArrowButton.setBounds(109, 632, 57, 140);
-
+		GridBagConstraints gbc_WestArrowButton = new GridBagConstraints();
+		gbc_WestArrowButton.fill = GridBagConstraints.BOTH;
+		gbc_WestArrowButton.gridx = 12;
+		gbc_WestArrowButton.gridy = 9;
+		contentPane.add(WestArrowButton, gbc_WestArrowButton);
+		
 		BasicArrowButton EastArrowButton = new BasicArrowButton(BasicArrowButton.EAST);
-		EastArrowButton.setBounds(1030, 632, 57, 140);
+		EastArrowButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_EastArrowButton = new GridBagConstraints();
+		gbc_EastArrowButton.fill = GridBagConstraints.BOTH;
+		gbc_EastArrowButton.gridx = 13;
+		gbc_EastArrowButton.gridy = 9;
+		contentPane.add(EastArrowButton, gbc_EastArrowButton);
 
-		container.add(WestArrowButton);
-		container.add(EastArrowButton);
-		container.add(imgScrollPanel);
-		container.add(inputPanel);
+//		container.add(WestArrowButton);
+//		container.add(EastArrowButton);
+//		container.add(imgScrollPanel);
+//		container.add(inputPanel);
 		// button to set neighbordialog visible
-		btnNeighborManage = new JButton(NEIGHBOR);
-		btnNeighborManage.setVisible(false);
+		
+		
+//		btnNeighborManage = new JButton(NEIGHBOR);
+//		btnNeighborManage.setVisible(false);
+		
+		JButton btnNeighborManage = new JButton(NEIGHBOR);
 		btnNeighborManage.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				neighborDialog.setVisible(true);
@@ -92,28 +153,32 @@ public class UserScreen extends JFrame {
 
 			}
 		});
-		btnNeighborManage.setBounds(0, 500, 40, 29);
-		getContentPane().add(btnNeighborManage);
+		GridBagConstraints gbc_btnNeighborManage = new GridBagConstraints();
+		gbc_btnNeighborManage.gridx = 15;
+		gbc_btnNeighborManage.gridy = 9;
+		contentPane.add(btnNeighborManage, gbc_btnNeighborManage);
+//		btnNeighborManage.setBounds(0, 500, 40, 29);
+//		getContentPane().add(btnNeighborManage);
 
-		setTitle(PATH);
-		setLocation(0, 0);
-		setSize(1200, 900);
+//		setTitle(PATH);
+//		setLocation(0, 0);
+		setSize(800, 500);
 		setVisible(true);
 		setResizable(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	public JButton getBtnNeighborManage() {
 		return this.btnNeighborManage;
 	}
 
-	public static UserScreen launchUserScreen() {
-		if (userScreen == null) {
-			userScreen = new UserScreen();
-		}
-
-		return userScreen;
-	}
+//	public static UserScreen launchUserScreen() {
+//		if (userScreen == null) {
+//			userScreen = new UserScreen();
+//		}
+//
+//		return userScreen;
+//	}
 
 	/**
 	 * The entrance of the program
@@ -121,16 +186,30 @@ public class UserScreen extends JFrame {
 	 * @param args
 	 *            command line..
 	 */
+//	public static void main(String[] args) {
+//
+//		// singleton
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//
+//				UserScreen.launchUserScreen();
+//			}
+//		});
+//
+//		new SystemFacade();
+//	}
+	
 	public static void main(String[] args) {
-
-		// singleton
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-
-				UserScreen.launchUserScreen();
+				try {
+					UserScreen frame = new UserScreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		});
-
 		new SystemFacade();
 	}
 
