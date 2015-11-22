4b4c6a267ab86af9e04f982c3513287f2c92b606
nakalamvokis <nkal123@gmail.com>
2015-11-22 17:27:28 -0500

    Adds new icons

--- BaseModel/src/com/wpi/cs509/teamA/ui/NodeIcon.java
+++ BaseModel/src/com/wpi/cs509/teamA/ui/NodeIcon.java
@@ -11,12 +11,21 @@ import com.wpi.cs509.teamA.util.NodeType;
 
 public class NodeIcon {
 	private Node node;
+
 	private BufferedImage redIcon;
 	private BufferedImage blueIcon;
+	private BufferedImage restroomIcon;
+	private BufferedImage parkingIcon;
+
 	private String redIconFilePath;
 	private String blueIconFilePath;
+	private String restroomIconFilePath;
+	private String parkingIconFilePath;
+
 	private String redIconName = "Red_Icon.png";
 	private String blueIconName = "Blue_Icon.png";
+	private String restroomIconName = "Restroom_Icon.png";
+	private String parkingIconName = "Parking_Icon.png";
 
 	public NodeIcon() {
 		CreateImages();
@@ -25,9 +34,13 @@ public class NodeIcon {
 	void CreateImages() {
 		this.redIconFilePath = System.getProperty("user.dir") + "/src/" + this.redIconName;
 		this.blueIconFilePath = System.getProperty("user.dir") + "/src/" + this.blueIconName;
+		this.restroomIconFilePath = System.getProperty("user.dir") + "/src/" + this.restroomIconName;
+		this.parkingIconFilePath = System.getProperty("user.dir") + "/src/" + this.parkingIconName;
 		try {
 			setRedIcon(ImageIO.read(new FileInputStream(this.redIconFilePath)));
 			setBlueIcon(ImageIO.read(new FileInputStream(this.blueIconFilePath)));
+			setRestroomIcon(ImageIO.read(new FileInputStream(this.restroomIconFilePath)));
+			setParkingIcon(ImageIO.read(new FileInputStream(this.parkingIconFilePath)));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -49,7 +62,8 @@ public class NodeIcon {
 	}
 
 	/**
-	 * @todo Fix this function to support multiple images for different node types
+	 * @todo Fix this function to support multiple images for different node
+	 *       types
 	 * @return the image
 	 */
 	public BufferedImage getImage(Node node) {
@@ -57,6 +71,12 @@ public class NodeIcon {
 		switch (type) {
 		case CLASSROOM: {
 			return this.redIcon;
+		}
+		case RESTROOM: {
+			return this.restroomIcon;
+		}
+		case PARKING: {
+			return this.parkingIcon;
 		}
 		default: {
 			return this.blueIcon;
@@ -80,5 +100,21 @@ public class NodeIcon {
 	public void setBlueIcon(BufferedImage image) {
 		this.blueIcon = image;
 	}
-	
+
+	/**
+	 * @param image
+	 *            the image to set
+	 */
+	public void setRestroomIcon(BufferedImage image) {
+		this.restroomIcon = image;
+	}
+
+	/**
+	 * @param image
+	 *            the image to set
+	 */
+	public void setParkingIcon(BufferedImage image) {
+		this.parkingIcon = image;
+	}
+
 }
