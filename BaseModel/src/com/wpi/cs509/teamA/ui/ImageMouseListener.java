a6dbde387b51d9dc27eef206f48ebe4224820cf0
Xiongyi Cui <Xiongyi Cui>
2015-11-24 15:44:49 -0500

    add MouseListener

---
+++ BaseModel/src/com/wpi/cs509/teamA/ui/ImageMouseListener.java
@@ -0,0 +1,48 @@
+package com.wpi.cs509.teamA.ui;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ * Created by cuixi on 11/24/2015.
+ */
+public class ImageMouseListener implements MouseListener {
+
+
+
+    private StateContext stateContext;
+
+    public ImageMouseListener() {
+        this.stateContext = null;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        stateContext.execute(e);
+    }
+
+    
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+
+
+    public void setStateContext(StateContext stateContext) {
+        this.stateContext = stateContext;
+    }
+
+
+}
