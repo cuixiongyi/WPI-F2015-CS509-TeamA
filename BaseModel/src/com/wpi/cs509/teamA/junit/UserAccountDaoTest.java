9d6146f7236e20fa2901671d4a08c3dbaf3f5881
yizhouyan <yizhouyan9132@outlook.com>
2015-11-28 20:30:49 -0500

    delete edge from database by two nodes

--- BaseModel/src/com/wpi/cs509/teamA/junit/UserAccountDaoTest.java
+++ BaseModel/src/com/wpi/cs509/teamA/junit/UserAccountDaoTest.java
@@ -1,24 +1,35 @@
 package com.wpi.cs509.teamA.junit;
 
+import com.wpi.cs509.teamA.bean.Node;
+
 //import static org.junit.Assert.*;
 
 //import org.junit.Test;
 
 import com.wpi.cs509.teamA.bean.UserAccount;
+import com.wpi.cs509.teamA.dao.NodeRelationDao;
 import com.wpi.cs509.teamA.dao.UserAccountDao;
+import com.wpi.cs509.teamA.dao.impl.NodeRelationDaoImpl;
 import com.wpi.cs509.teamA.dao.impl.UserAccountDaoImpl;
 
 public class UserAccountDaoTest {
 
 	//@Test
-	public void test() {
-		UserAccount ua = new UserAccount();
+	public static void main(String [] args) {
+	/*	UserAccount ua = new UserAccount();
 		ua.setUsername("yizhouyan");
 		ua.setPassword("hammer");
 		ua.setAdmin(true);
 		ua.setEmail("yyan2@wpi.edu");
 		UserAccountDao uad = new UserAccountDaoImpl();
-		uad.addAccountToDatabase(ua);
+		uad.addAccountToDatabase(ua);*/
+		
+		NodeRelationDao nrd = new NodeRelationDaoImpl();
+		Node n1 = new Node();
+		n1.setId(100);
+		Node n2 = new Node();
+		n2.setId(20);
+		System.out.println(nrd.deleteEdge(n1, n2));
 		}
 
 }
