dc19dc22566f33cbcb8179d0bf19588f506a2840
Xiongyi Cui <cuixiongyi2013@gmail.com>
2015-11-30 19:19:30 -0500

    add NodeSetMune

---
+++ BaseModel/src/com/wpi/cs509/teamA/ui/NodeSetMenu.java
@@ -0,0 +1,71 @@
+package com.wpi.cs509.teamA.ui;
+
+import javax.swing.JPopupMenu;
+import javax.swing.JTextField;
+
+import com.wpi.cs509.teamA.bean.Node;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JMenuItem;
+
+/**
+ * This is the class that administrators uses to add nodes to database
+ * 
+ * @author CS 509-Team A
+ *
+ */
+
+@SuppressWarnings("serial")
+public class NodeSetMenu extends JPopupMenu implements ActionListener {
+	private Node node;
+	private InputPanel inputPanel;
+	private JMenuItem mntmSrc;
+	private JMenuItem mntmDes;
+	private StateContext stateContext;
+
+	private final static String SOURCE = "Add as source";
+	private final static String DES = "Add as destination";
+
+	/**
+	 * Create the Menu.
+	 */
+	public NodeSetMenu(InputPanel inputPanel, Node node, StateContext stateContext) {
+		this.node = node;
+		this.inputPanel = inputPanel;
+		this.stateContext = stateContext;
+
+		setBounds(100, 100, 450, 300);
+
+		// set up menu item
+		mntmSrc = new JMenuItem(SOURCE);
+		mntmSrc.addActionListener(this);
+		add(mntmSrc);
+
+		mntmDes = new JMenuItem(DES);
+		add(mntmDes);
+		mntmDes.addActionListener(this);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == mntmSrc) {
+
+			this.inputPanel.getFromText().setText(node.getMap().getMapName()+" "+node.getName());
+			this.stateContext.setStartNode(node);
+			this.inputPanel.getAutoSuggestorFrom().getAutoSuggestionPopUpWindow().setVisible(false);
+					
+		} else if (e.getSource()==mntmDes){
+			this.inputPanel.getToText().setText(node.getMap().getMapName()+" "+node.getName());
+			this.stateContext.setEndNode(node);
+			this.inputPanel.getAutoSuggestorTo().getAutoSuggestionPopUpWindow().setVisible(false);
+		}
+		
+	}
+
+	public void setNode(Node node, JTextField txt) {
+
+	}
+}
