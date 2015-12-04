a7d08824797c360009112b89776bc1492ed10292
xiongkuang <xiongkuang@gmail.com>
2015-12-03 21:32:41 -0500

    absoluted

--- BaseModel/src/com/wpi/cs509/teamA/ui/UserScreen.java
+++ BaseModel/src/com/wpi/cs509/teamA/ui/UserScreen.java
@@ -15,11 +15,14 @@ import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+
 import com.wpi.cs509.teamA.bean.GeneralMap;
 //import com.sun.prism.paint.Color;
 import com.wpi.cs509.teamA.util.Database;
 import com.wpi.cs509.teamA.util.PaintHelper;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 
@@ -76,91 +79,37 @@ public class UserScreen extends JFrame {
 		}
 
 		UIManager.put("nimbusBase", new Color(50, 50, 50));
-		 UIManager.put("ComboBox:\"ComboBox.listRenderer\".background", new Color(142, 143, 145));
-		 UIManager.put("control", new Color(142, 143, 145));
-		 UIManager.put("text", new Color(255,255,255));
-		 UIManager.put("TextField.background", new Color(180, 180, 180));
-
+	    UIManager.put("ComboBox:\"ComboBox.listRenderer\".background", new Color(142, 143, 145));
+		UIManager.put("control", new Color(142, 143, 145));
+		UIManager.put("text", new Color(255,255,255));
+		UIManager.put("TextField.background", new Color(180, 180, 180));
         UIManager.put("List.background", new Color(180, 180, 180));
         UIManager.put("PasswordField.background", new Color(180, 180, 180));
-		container = getContentPane();
-		// container.setLayout(new BorderLayout());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
 		
-		
-		this.setBounds(100, 100, 800, 600);
+		this.setBounds(50, 0, 1200, 770);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-//		GridBagLayout gblContentPane = new GridBagLayout();
-//		gblContentPane.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30, 30, 50, 50 };
-//		gblContentPane.rowHeights = new int[] { 0 };
-//		gblContentPane.columnWeights = new double[] { Double.MIN_VALUE };
-//		gblContentPane.rowWeights = new double[] { Double.MIN_VALUE };
-//		contentPane.setLayout(gblContentPane);
-
-		// input panel and components
 
 		inputPanel = new InputPanel();
-		inputPanel.setBounds(600, 0, 300, 600);
-//		GridBagConstraints gbcInputPanel = new GridBagConstraints();
-//		gbcInputPanel.gridwidth = 7;
-//		// gbcInputPanel.gridheight = GridBagConstraints.RELATIVE;
-//		gbcInputPanel.insets = new Insets(0, 0, 5, 5);
-//		gbcInputPanel.fill = GridBagConstraints.BOTH;
-//		gbcInputPanel.gridx = 10;
-//		gbcInputPanel.gridy = 0;
-//		gbcInputPanel.weightx = 0.1;
-
+		inputPanel.setBounds(905, 30, 300, 750);
 		contentPane.add(inputPanel);
-
-		// initialize image block, wrapping panel to limit size of image
-		// component
-//		wrappingImgPanel = new JPanel();
-//		wrappingImgPanel.setBounds(0, 0, 500, 500);
-//		wrappingImgPanel.setMaximumSize(new Dimension(1024, 1024));
-////		GridBagConstraints gbcwrappingImgPanel = new GridBagConstraints();
-////		gbcwrappingImgPanel.insets = new Insets(0, 0, 5, 5);
-////		gbcwrappingImgPanel.fill = GridBagConstraints.BOTH;
-////		gbcwrappingImgPanel.gridx = 0;
-////		gbcwrappingImgPanel.gridy = 0;
-////		gbcwrappingImgPanel.weightx = 0.6;
-////		gbcwrappingImgPanel.weighty = 0.6;
-//		contentPane.add(wrappingImgPanel);
-//		wrappingImgPanel.setLayout(new BoxLayout(wrappingImgPanel, BoxLayout.X_AXIS));
 
 		// the panel to show image
 		imgComponent = new ImageComponent();
-		imgComponent.setMaximumSize(new Dimension(1024, 1024));
 		contentPane.add(imgComponent);
-		imgComponent.setBounds(0, 0, 500, 500);
+		imgComponent.setBounds(0, 0, 900, 750);
+		imgComponent.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
 
 		
-//		JScrollPane imgScrollPanel = new JScrollPane();
-//		imgScrollPanel.setMaximumSize(new Dimension(1024, 1024));
-//		// contentPane.add(imgScrollPanel, gbcScrollPane);
-//		// // for scroll panel
-//		imgScrollPanel.setViewportView(imgComponent);
-//		imgScrollPanel.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-//		imgScrollPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-//
-//		wrappingImgPanel.add(imgScrollPanel);
-
-//		setSize(800, 500);
 		setVisible(true);
-		setResizable(true);
+		setResizable(false);
 
-		// TODO: default map? change the way to do this...
-		// TODO load map image with different names
-
-	/*	
-		imgComponent.setImagePath(System.getProperty("user.dir") + "/src/Final_Campus_Map.jpg");
-		imgComponent.setPreferredSize(new Dimension(imgComponent.getImgWidth(), imgComponent.getImgHeight()));
-		imgComponent.setVisible(true);
-*/
-		
 		imgComponent.setInputPanel(this.inputPanel);
         inputPanel.setImageComponent(this.imgComponent);
 
