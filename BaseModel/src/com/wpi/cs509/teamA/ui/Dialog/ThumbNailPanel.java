63f96e7d73c98df0a7d1873d30959e59b367eb11
xiongkuang <xiongkuang@gmail.com>
2015-12-13 20:37:55 -0500

    edited thumb

--- BaseModel/src/com/wpi/cs509/teamA/ui/Dialog/ThumbNailPanel.java
+++ BaseModel/src/com/wpi/cs509/teamA/ui/Dialog/ThumbNailPanel.java
@@ -17,16 +17,18 @@ import javax.swing.ImageIcon;
 /**
  * Created by xiongkuang on 12/13/15.
  */
-public class ThumbNailPanel extends JScrollPane implements ActionListener {
+public class ThumbNailPanel extends JScrollPane implements MouseListener {
     private MainModel model;
     private List<Path> paths;
     private List<JLabel> icons;
     private List<JLabel> iconTexts;
-    private int resizeX = 40;
-    private int resizeY = 40;
-    private int picInset = 100;
+    private int resizeX = 200;
+    private int resizeY = 200;
+    private int picInset = 300;
 
     public  ThumbNailPanel(MainModel model){
+        this.setSize(800,220);
+        this.setLocation(110,110);
         this.model = model;
         this.icons = new ArrayList<JLabel>();
         this.iconTexts = new ArrayList<JLabel>();
@@ -41,39 +43,69 @@ public class ThumbNailPanel extends JScrollPane implements ActionListener {
         int picX=0;
         int picY=0;
         int textX=0;
-        int textY=100;
+        int textY=205;
 
         for(Path newPath : this.paths)
         {
             JLabel newIcon = new JLabel(new ImageIcon(NodeIcon.resize(newPath.getMap().getImage(),resizeX,resizeY)));
             JLabel newText = new JLabel(newPath.getMap().getMapName());
+            newIcon.addMouseListener(this);
             icons.add(newIcon);
             iconTexts.add(newText);
 
-
             newIcon.setBounds(picX,picY,resizeX,resizeY);
-            newText.setBounds(textX,textY,resizeX,resizeY);
+            newText.setBounds(textX,textY,200,30);
 
             picX=picX+picInset;
+            textX=textX+picInset;
 
             this.add(newIcon);
             this.add(newText);
+            System.out.println("Made a new icon: " + newText.getText());
         }
         setCurrentMap(0);
     }
 
     void setCurrentMap(int index)
     {
-        model.setCurrentPath(index);
+        this.model.setCurrentPath(index);
     }
-
-
 
 
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        setCurrentMap(icons.indexOf(e.getSource().getClass()));
+    public void mouseClicked(MouseEvent e) {
+
+        int idx = icons.indexOf(e.getSource());
+        setCurrentMap(idx);
+
     }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+
+
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//
+//    }
 
 }
