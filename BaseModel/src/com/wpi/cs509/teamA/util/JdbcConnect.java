656c7d5070cb263661580da2bd2ae52aa11b8ab8
Xiongyi Cui <cuixiongyi2013@gmail.com>
2015-12-15 13:26:36 -0500

    test driver is wrong

--- BaseModel/src/com/wpi/cs509/teamA/util/JdbcConnect.java
+++ BaseModel/src/com/wpi/cs509/teamA/util/JdbcConnect.java
@@ -18,7 +18,7 @@ public class JdbcConnect {
 
 	// TODO: Make this in the file
 	private static String url = "jdbc:mysql://localhost:3306/routefinder";
-	private static String password = "root";
+	private static String password = "123456";
 
 
 	//private static String url = "jdbc:mysql://130.215.28.216:3306/routefinder";
